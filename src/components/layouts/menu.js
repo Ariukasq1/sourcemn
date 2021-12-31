0480da5088d943f0f5fa442c68bef98ae7444e4d
Gift_zwerg3 <bulgantamir@nmma.co>
2021-12-31 12:38:26 +0800

    update

--- src/components/layouts/menu.js
+++ src/components/layouts/menu.js
@@ -1,31 +1,15 @@
 import React from "react";
-import axios from 'axios';
-import Config from '../../config';
 
 class MenuComponent extends React.Component {
-  state = { headerMenu: [] };
-
-  componentDidMount() {
-    axios
-      .get(`${Config.apiUrl}/menus/v1/menus/header-menu`)
-      .then(res =>
-        this.setState({
-          headerMenu: res.data,
-          loading: true
-        })
-      )
-      .catch(err => console.log(err))
-  }
-
   render() {
     return (
       <div className="main-header">
-        Here will be menu
+        <div className="logo">
+          <img src="/images/mms.png" />
+        </div>
       </div>
     );
   }
 }
 
 export default MenuComponent;
-
-
