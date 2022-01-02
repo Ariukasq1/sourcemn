96114b74bd9a8bc139f0bce398d1407eaa038126
Gift_zwerg3 <bulgantamir@nmma.co>
2022-01-03 03:59:07 +0800

    update

--- src/pages/[pages]/[detail]/index.js
+++ src/pages/[pages]/[detail]/index.js
@@ -2,12 +2,34 @@ import React from "react";
 import Layout from "../../../components/layouts/Layout";
 import config, { fetcher } from "../../../config";
 import WPAPI from "wpapi";
+import FirstPart from "../../../components/indCap/firstPart";
 
-const Detail = ({ mainMenu, topMenu, data }) => {
-  console.log(data, "-----------");
+const Detail = ({ mainMenu, topMenu, data, slug }) => {
+  const renderData = () => {
+    switch (slug) {
+      case "brands":
+        return <>brandsDetail</>;
+      case "career":
+        return <>careerDetail</>;
+      case "news":
+        return <>newsDetail</>;
+      case "portfolio":
+        return (
+          <>
+            <FirstPart clas={slug} data={data} />
+          </>
+        );
+      default:
+        return (
+          <>
+            <FirstPart clas={slug} data={data} />
+          </>
+        );
+    }
+  };
   return (
     <Layout mainMenu={mainMenu} topMenu={topMenu}>
-      <div className="page">detail</div>
+      <div className="page">{renderData()}</div>
     </Layout>
   );
 };
@@ -15,13 +37,13 @@ const Detail = ({ mainMenu, topMenu, data }) => {
 Detail.getInitialProps = async (context) => {
   const wp = new WPAPI({ endpoint: config(context).apiUrl });
 
-  const slug = context.query.slug;
+  const slug = context.query.pages;
 
   const detail = context.query.detail;
 
   const catId = await wp
     .categories()
-    .slug(`${detail}`)
+    .slug(`${slug}`)
     .embed()
     .then((data) => data[0]);
 
@@ -36,7 +58,7 @@ Detail.getInitialProps = async (context) => {
     `${config(context).apiUrl}/menus/v1/menus/nav-menu-top`
   );
 
-  return { mainMenu, topMenu, data };
+  return { mainMenu, topMenu, data, slug };
 };
 
 export default Detail;
