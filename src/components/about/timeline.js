c8761bdab9512246e5d2bcf0bc69f2bedd09c118
Gift_zwerg3 <bulgantamir@nmma.co>
2022-01-24 19:05:41 +0800

    update

--- src/components/about/timeline.js
+++ src/components/about/timeline.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Slider from "react-slick";
+import { __ } from "../../utils";
 
 const TimeLine = ({ timeline }) => {
-  console.log(timeline, "-----------");
   const settings = {
     dots: false,
     slidesToShow: 5,
@@ -10,16 +10,22 @@ const TimeLine = ({ timeline }) => {
     autoplay: true,
     speed: 1000,
     autoplaySpeed: 2000,
+    arrows: false,
   };
 
   return (
     <div className="timeline">
+      <div className="sub-title">{__("Our History")}</div>
       <Slider {...settings} className="slider-time">
         {timeline.map((item, ind) => {
           return (
             <div key={ind} className="time">
               <h1>{item.acf.year}</h1>
+              <div className="arrow-timeline">
+                <img src="/images/timeline-arrow.png" />
+              </div>
               <div
+                className="content"
                 dangerouslySetInnerHTML={{ __html: item.content.rendered }}
               />
             </div>
