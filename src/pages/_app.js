ed1d8885c766494cadcd09ad4b926dabcc84c014
Ujin's MacBook <macintosh@Macintoshs-MacBook-Pro.local>
2022-01-27 19:39:57 +0800

    add translations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,56 @@
 import React from "react";
-import "../public/styles/style.min.css";
-import "../public/styles/fontawesome/css/all.min.css";
-import "slick-carousel/slick/slick.css";
+import App from "next/app";
 import Router from "next/router";
 import NProgress from "nprogress";
+import { getLang } from "../config";
+import { setLocale } from "../utils";
 import AOS from "aos";
+
 import "aos/dist/aos.css";
+import "slick-carousel/slick/slick.css";
+import "../public/styles/style.min.css";
+import "../public/styles/fontawesome/css/all.min.css";
 
-Router.events.on("routeChangeStart", () => {
-  NProgress.start();
-});
-
-Router.events.on("routeChangeComplete", () => {
-  NProgress.done();
-});
-
-Router.events.on("routeChangeError", () => NProgress.done());
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, lang }) {
   React.useEffect(() => {
     AOS.init({ duration: 800 });
     AOS.init({ disable: "mobile" });
     AOS.refresh();
   }, []);
+
+  const [loading, setLoading] = React.useState(false);
+  const [isLangSetted, setLangState] = React.useState(false);
+
+  if (!isLangSetted) {
+    setLocale(lang || "en", () => {
+      setLangState(true);
+    });
+  }
+
+  React.useEffect(() => {
+    const handleStart = () => NProgress.start() && setLoading(true);
+
+    // handleComplete event was not firing
+    const handleComplete = () => NProgress.done() && setLoading(false);
+
+    AOS.init({ duration: 800 });
+    AOS.init({ disable: "mobile" });
+    AOS.refresh();
+
+    Router.events.on("routeChangeStart", handleStart);
+    Router.events.on("routeChangeComplete", handleComplete);
+    Router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleStart);
+      Router.events.off("routeChangeComplete", handleComplete);
+      Router.events.off("routeChangeError", handleComplete);
+    };
+  }, []);
+
+  if (!isLangSetted) {
+    return null;
+  }
 
   return (
     <div className="next">
@@ -30,5 +58,12 @@ function MyApp({ Component, pageProps }) {
     </div>
   );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps, lang: getLang(appContext.ctx) };
+};
 
 export default MyApp;
